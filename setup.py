03e83131850a9d6cadcdb0c2e9eaae342c9d3947
roshan9900 <roshansalunke91@gmail.com>
2025-09-10 13:20:44 +0530

    created the setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as file:
+        requirements=file.readlines()
+        requirements= [req.replace('\n','') for req in requirements]
+
+        if '-e .' in requirements:
+
+            requirements.remove('-e .')
+    return requirements
+
+
+setup(
+    name='AdTracking-Fraud-Detection',
+    version='0.0.1',
+    author='Roshan Salunke',
+    author_email='roshansalunke91@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
